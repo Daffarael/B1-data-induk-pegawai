5bdc211b77d497a0a30b660942d5238f19b79a7f
luthfi harisna mufti <luthfihm024@gmail.com>
2026-06-09 11:22:48 +0700

    memperbaiki struktur jabatan

--- routes/api/strukturJabatan.js
+++ routes/api/strukturJabatan.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const c = require('../../controllers/strukturJabatanController');
+const { isAuthenticated } = require('../../middlewares/auth');
+const { hasRole } = require('../../middlewares/acl');
 
-// REST API endpoint - biasanya diproteksi dengan API Key / token
-// Untuk simulasi ini, kita anggap terbuka atau bisa ditambahkan middleware lain
+const guard = [isAuthenticated, hasRole('Admin Kepegawaian')];
 
-router.get('/', c.index);
-router.post('/', c.store);
-router.get('/:id', c.show);
-router.put('/:id', c.update);
-router.delete('/:id', c.destroy);
+// REST API endpoint - dilindungi ACL sesuai role
 
-router.post('/:id/tupoksi', c.storeTupoksi);
-router.put('/:id/tupoksi/:tupoksi_id', c.updateTupoksi);
-router.delete('/:id/tupoksi/:tupoksi_id', c.destroyTupoksi);
+router.get('/', guard, c.index);
+router.post('/', guard, c.store);
+router.get('/:id', guard, c.show);
+router.put('/:id', guard, c.update);
+router.delete('/:id', guard, c.destroy);
+
+router.post('/:id/tupoksi', guard, c.storeTupoksi);
+router.put('/:id/tupoksi/:tupoksi_id', guard, c.updateTupoksi);
+router.delete('/:id/tupoksi/:tupoksi_id', guard, c.destroyTupoksi);
 
 module.exports = router;
